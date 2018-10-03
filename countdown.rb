2347bb8b9e6727660db235f478ca1d49502c24a5
Megan Farley <megfarley4@gmail.com>
2018-10-03 01:28:00 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,12 @@ def countdown(int)
   "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(int)
+  counter = int
+  while counter > 0
+  puts "#{counter} SECOND(S)!"
+  
+  counter -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
